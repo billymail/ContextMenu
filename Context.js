3dbd48a06a542f318204e32ef4dfa90b8703098e
billymail <billcreswell@ymail.com>
2016-02-29 14:29:57 -0500

    remove need for MT Compatibility Mode
    Update type and bindWithEvents to remove need for MT Compatibility Mode

--- Context.js
+++ Context.js
@@ -1,14 +1,15 @@
 /**
- * Context
- * slightly based on Simple Context Menu from [url=http://www.webtoolkit.info]http://www.webtoolkit.info[/url]
+ * @brief JS Context Menu
  *
+ * Based on Simple Context Menu
+ * [url=http://www.webtoolkit.info] http://www.webtoolkit.info[/url]
  * By Pieter-Jan Piro - [url=http://www.piro.ws]http://www.piro.ws[/url]
- *
- * Touch Handling added 2016
+ * 
+ * Update with simple touch events, and for new MooTools without Compatibility
  *
  **/
 var Context = new Class({
-    getOptions: function(){
+    getOptions: function() {
         return {
             menus: [],
             offsets: {'x': 5, 'y': 5},
@@ -20,31 +21,42 @@ var Context = new Class({
             }
         }
     },
+/**
+ * @brief Initialize
+ */
     initialize: function(options){
-        /** load the context menu css */
-        if (!$('CONTEXT_CSS')) new Asset.css('/js/widgets/Context/css/Context.css', {id: 'CONTEXT_CSS'});
-
+    // load the context menu css
+        if (!$('CONTEXT_CSS')) {
+            new Asset.css('/js/widgets/Context/css/Context.css', {id: 'CONTEXT_CSS'});
+        }
         this.setOptions(this.getOptions(), options);
         // if (Browser.Engine.trident || Browser.Engine.gecko){
-            document.addEvent('mousedown', this.show.bindWithEvent(this));
-            document.addEvent('contextmenu', this.hideContext.bindWithEvent(this));
+            var self = this;
+            document.addEvent('mousedown', function(e) {
+                self.showContext(e, this);
+            });
+            document.addEvent('contextmenu', function(e) {
+                self.hideContext(e, this);
+            });
         // }
     },
-    show: function(e){
+/**
+ * @brief Show Context Menu
+ */
+    showContext: function(e){
         if (this.currentMenu){
-            //check if childnode from menu is clicked...
+            //check if childnode from menu is clicked...Older IE Doesn't Support
             var tipPos = {'x': this.currentMenu.getStyle('left').toInt(), 'y': this.currentMenu.getStyle('top').toInt()};
             var tip = {'x': this.currentMenu.offsetWidth, 'y': this.currentMenu.offsetHeight};
             if (!(tipPos.x < e.page.x && e.page.x < tipPos.x+tip.x && tipPos.y < e.page.y && e.page.y < tipPos.y+tip.y )){
-                this.hide();
+              this.hide();
             }
         }
-
         if (e.rightClick || ('ontouchstart' in document.documentElement)){
             //show contextmenu...
             var el = e.target;
 
-            if ($type(el.getProperty('Context')) != 'false' && this.options.menus[el.getProperty('Context')]){
+            if (typeOf(el.getProperty('Context')) != 'false' && this.options.menus[el.getProperty('Context')]){
                 this.currentMenu = this.options.menus[el.getProperty('Context')];
                 this.currentEl = el;
                 this.locate(e);
@@ -53,17 +65,25 @@ var Context = new Class({
                 return false;
             }
         }
-       if ('ontouchstart' in document.documentElement) {
+        if ('ontouchstart' in document.documentElement) {
             return false;
         }
+
         return true;
     },
+/**
+ * @brief Hide Context Options
+ */
     hide: function(){
         this.options.onHide(this.currentMenu, this.currentEl);
     },
+/**
+ * @brief Hide Context Menu
+ */
     hideContext: function(e){
         var el = e.target;
-        if ($type(el.getProperty('Context')) != 'false' && this.options.menus[el.getProperty('Context')] || el == this.currentMenu){
+        if (typeOf(el.getProperty('Context')) != 'false'
+            && this.options.menus[el.getProperty('Context')] || el == this.currentMenu){
             e.stop();
             return false;
         }
@@ -76,7 +96,7 @@ var Context = new Class({
         }
     },
     attachMenu: function(el, menu){
-        if ($type(menu) == 'element'){
+        if (typeOf(menu) == 'element'){
             var menuId = this.options.menus.push(menu)-1;
         } else if ($type(menu) == 'number'){
             var menuId = menu;
